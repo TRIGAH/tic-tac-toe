392b951572739e140ce5ea8b62e96cd69c1cc00c
Trigah <ijenrandy2@gmail.com>
2024-01-24 16:05:33 +0100

    Add a Player and Computer feature

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random
 
-game_board = [['','',''],['','',''],['','','',]]
-
-
+game_board = [['','',''],['','',''],['','','']]
+PLAYERS = ['X','O']
+POSITION= ''
 
 def check_full_board(board):
     return all([all(board[i]) for i in  range(len(board)) ])
@@ -12,11 +12,25 @@ def print_board(board):
         print(" | ".join(row))
         print("-" * 9)
 
-def play_game(board):
+def generated_choices(board):
+    choosen_player = random.choice(PLAYERS)
     choose_row = random.choice(range(len(board)))
     choose_row_position = random.choice(range(len(board[choose_row])))
-    board[choose_row][choose_row_position] = 'x'
-    print_board(board)
+    board[choose_row][choose_row_position] = 'X'
+
+    choose_row_o = random.choice(range(len(board)))
+    choose_row_position_o = random.choice(range(len(board[choose_row_o])))
+    if board[choose_row_o][choose_row_position_o] == 'X':
+        board[choose_row_o][choose_row_position_o] = 'O'
+    else:
+        board[choose_row_o][choose_row_position_o] = 'O'
+   
+
+
+
+def play_game(board):
+    generated_choices(board)
+    print_board(board)    
 
 if check_full_board(game_board):
     print("Game Over")  
