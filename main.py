305f007faa25544e59d0cb7aca0a302a5873bbe9
Trigah <ijenrandy2@gmail.com>
2024-01-25 14:12:33 +0100

    Add move feature

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ game_board = [['','',''],['','',''],['','','']]
 PLAYERS = ['X','O']
 POSITION= ''
 
-def check_full_board(board):
+
+
+def check_board(board):
     return all([all(board[i]) for i in  range(len(board)) ])
 
 def print_board(board):
@@ -13,27 +15,33 @@ def print_board(board):
         print("-" * 9)
 
 def generated_choices(board):
-    choosen_player = random.choice(PLAYERS)
-    choose_row = random.choice(range(len(board)))
-    choose_row_position = random.choice(range(len(board[choose_row])))
-    board[choose_row][choose_row_position] = 'X'
+    if check_board(board) == True:
+       print("Game Over")  
+    else:      
+        choosen_player = random.choice(PLAYERS)
+        choose_row = random.choice(range(len(board)))
+        choose_row_position = random.choice(range(len(board[choose_row])))
+        board[choose_row][choose_row_position] = 'X'
 
-    choose_row_o = random.choice(range(len(board)))
-    choose_row_position_o = random.choice(range(len(board[choose_row_o])))
-    if board[choose_row_o][choose_row_position_o] == 'X':
-        board[choose_row_o][choose_row_position_o] = 'O'
-    else:
-        board[choose_row_o][choose_row_position_o] = 'O'
+        choose_row_o = random.choice(range(len(board)))
+        choose_row_position_o = random.choice(range(len(board[choose_row_o])))
+        if board[choose_row_o][choose_row_position_o] == 'X':
+            board[choose_row_o][choose_row_position_o] = 'O'
+        else:
+            board[choose_row_o][choose_row_position_o] = 'O'
    
 
+def move(board):
+    while check_board(board) == False:
+        move_x = input('Enter 1 to move: ')
+        if move_x == '1':
+            generated_choices(board)
+            print_board(board)
+
+move(game_board)        
+ 
+ 
+         
 
 
-def play_game(board):
-    generated_choices(board)
-    print_board(board)    
-
-if check_full_board(game_board):
-    print("Game Over")  
-else:
-    play_game(game_board)
 
